bb99745090e607a10200e8519a617060fc415717
boyinblue <boyinblue@naver.com>
2022-05-01 09:32:00 +0900

    update

--- tts_client.py
+++ tts_client.py
@@ -7,21 +7,34 @@ import time
 pipe_path = "/tmp/mp3_player"
 pipe = None
 
+def check_server_side():
+    print("Check tts service")
+    for line in os.popen("sudo -H -u parksejin bash -c 'systemctl --user --machine=parksejin@.host status tts'"):
+        print(line)
+
+#    os.
+#    sudo -H -u otheruser bash -c 'echo "I am $USER, with uid $UID"'
+
 def pipe_init():
     global pipe
 
     if not os.path.exists(pipe_path):
         os.mkfifo(pipe_path)
-    os.chmod(pipe_path, 0o666)
+        os.chmod(pipe_path, 0o666)
     pipe_fd = os.open(pipe_path, os.O_RDWR | os.O_NONBLOCK)
     pipe = os.fdopen(pipe_fd, 'w')
+
+    check_server_side()
 
 def pipe_speak(message):
     if not pipe:
         pipe_init()
 
     pipe.write(message + "\n")
-    pipe.flush()
+    try:
+        pipe.flush()
+    except BlockingIOError:
+        print("Unable to flush")
 
 def main():
     pipe_speak("반갑습니다.")
--- tts_main.py
+++ tts_main.py
@@ -8,23 +8,51 @@ menu_item = ["오늘 날짜",
                 "음악" ]
 
 menu = None
+path = None
 
 pipe_path = "/tmp/tts_pipe"
-mp3_path = "/home/parksejin/Music/LittleBusTayo4"
+mp3_path = "/home/parksejin/Music/"
 
-def play_mp3():
-    filenames = os.listdir(mp3_path)
+def list_mp3(path = mp3_path):
+    global menu
+
+    filenames = os.listdir(path)
+    menu = filenames
+    for idx in range(0, len(filenames)):
+        msg = "{}번 {}".format(idx, filenames[idx])
+        tts_client.pipe_speak(msg)
+
+def list_mp3_parser(key):
+    if key == "KEY_0":
+        path = path + "/" + menu[0]
+    elif key == "KEY_1":
+        path = path + "/" + menu[1]
+    
+    list_mp3(path)
+
+def play_mp3(path = mp3_path):
+    filenames = os.listdir(path)
     for filename in filenames:
+        if filename == "." or filename == "..":
+            continue
         print("filename :", filename)
+        full_filename = os.path.join(path, filename)
+        if os.path.isdir(full_filename):
+            play_mp3(full_filename)
+            continue
+
         print("ext :", filename[-4:])
         if filename[-4:].lower() != ".mp3":
             continue
-        full_filename = os.path.join(mp3_path, filename)
+
         print(full_filename)
         tts_client.pipe_speak(full_filename)
 
 def parse_key(key):
-    if key == "KEY_MENU" and not menu:
+    global menu, path
+    if key == "KEY_MENU":
+        menu = None
+        path = None
         tts_client.pipe_speak("메뉴")
         for i in range(len(menu_item)):
             tts_msg = "{}번 {}".format(i, menu_item[i])
@@ -34,11 +62,17 @@ def parse_key(key):
     elif key == "KEY_1":
         tts_datetime.play_time()
     elif key == "KEY_2":
-        pass
+        list_mp3()
     elif key == "KEY_3":
         play_mp3()
     elif key == "KEY_STOP":
         tts_client.pipe_speak("clear")
+    elif key == "KEY_NEXT":
+        tts_client.pipe_speak("next")
+    elif key == "KEY_PLAY":
+        tts_client.pipe_speak("play")
+    elif key == "KEY_PAUSE":
+        tts_client.pipe_speak("pause")
     else:
         print("Cannot handle key : '{}'".format(key))
 
